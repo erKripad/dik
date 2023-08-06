88d4d2a7d123e92378d14300913ab56828ea24f5
EnricoChen42 <94288445+EnricoChen42@users.noreply.github.com>
2023-08-07 00:16:29 +0200

    Inizializzazione e configurazione di Firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:dik/Theme/my_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 //----------------------------------------------------------------------------//
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );  
   runApp(const MyApp());
 }
 
