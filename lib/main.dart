ace8e78e7a99ec982c470a2fdac5dd3d1a74a6bc
Tizio777 <jiaxingchen777@gmail.com>
2023-08-16 13:37:14 +0200

    Prova

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );  
+  );
   runApp(const MyApp());
 }
 
